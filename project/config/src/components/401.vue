30352fcff1067210184b32c1df7fc38509a16f8d
在线的青春 <36788475+wenxuesay@users.noreply.github.com>
2018-06-15 20:39:12 +0800

    Create 401.vue

---
+++ project/config/src/components/401.vue
@@ -0,0 +1,29 @@
+<template lang="pug">
+  section.sf-content
+    .sf-panel
+      .panel-heading 401
+      .panel-body
+        h4 您没有访问权限,{{time}}秒后跳转
+</template>
+<script>
+export default {
+  data() {
+    return {
+      time: 3,
+    };
+  },
+  methods: {
+  },
+  mounted() {
+    const $vm = this;
+    setInterval(() => {
+      $vm.time -= 1;
+      if ($vm.time === 0) {
+        $vm.$router.replace({
+          path: '/',
+        });
+      }
+    }, 1000);
+  },
+};
+</script>
