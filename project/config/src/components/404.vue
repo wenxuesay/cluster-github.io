036445ea159975e9f3110de5a86a6643e2052921
在线的青春 <36788475+wenxuesay@users.noreply.github.com>
2018-06-15 20:39:34 +0800

    Create 404.vue

---
+++ project/config/src/components/404.vue
@@ -0,0 +1,29 @@
+<template lang="pug">
+  section.sf-content
+    .sf-panel
+      .panel-heading 404
+      .panel-body
+        h4 您访问页面不存在,{{time}}秒后跳转
+</template>
+<script>
+export default {
+  data() {
+    return {
+      time: 3,
+    };
+  },
+  methods: {
+  },
+  mounted() {
+    const $vm = this;
+    setInterval(() => {
+      $vm.time -= 1;
+      if ($vm.time === 0) {
+        $vm.$router.replace({
+          path: '/',
+        });
+      }
+    }, 1000);
+  },
+};
+</script>
